5a771467a9c3d0ea9a32fd9b5cac51d4653d67f8
Tom Virgo <t.virgo@iwoca.co.uk>
2022-08-12 13:52:26 +0100

    add pop up message

--- src/components/PopUpMessage/index.js
+++ src/components/PopUpMessage/index.js
@@ -1,26 +1,39 @@
 import { useState, useEffect } from "react"
 
-const PopUpMessage = ({ message }) => {
+const PopUpMessage = ({ message, messageType, setter }) => {
     const [show, setShow] = useState(true)
 
     useEffect(() => {
         const timeId = setTimeout(() => {
             setShow(false)
-        }, 4000)
+            setter("")
+        }, 6000)
 
         return () => {
             clearTimeout(timeId)
         }
-    }, [])
+    }, [setter])
 
     if (!show) {
         return null;
     }
 
     return (
-        <div>
-            <h1>{message}</h1>
-        </div>
+        <>
+            {messageType === "success" &&
+                <div className="badge badge-success gap-2 fixed inset-x-0 top-2 mx-auto">
+                    <svg onClick={() => setShow(false)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                    {message}
+                </div>
+            }
+            {messageType === "error" &&
+                <div className="badge badge-error gap-2 fixed inset-x-0 top-2 mx-auto">
+                    <svg onClick={() => setShow(false)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                    {message}
+                </div>
+            }
+
+        </>
     )
 }
 
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,30 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-
 import { useAuth } from '../../contexts/auth'
+import { PopUpMessage } from '../../components'
 
 function SignUp() {
     const [error, setError] = useState(null)
     const { signUp } = useAuth()
-    const [input, setInput] = useState({
+    const baseInput = {
         "email": "",
         "password": "",
-        "repeat-password": "",
-    })
+        "repeatPassword": "",
+    }
+    const [input, setInput] = useState(baseInput)
+    const [sucessMessage, setSuccessMessage] = useState("")
 
     async function handleSubmit(e) {
         e.preventDefault()
+        if (input.password !== input.repeatPassword) {
+            setError("passwords must match, please try again")
+            setInput(baseInput)
+            return
+        }
         const { error } = await signUp({ "email": input.email, "password": input.password })
         if (error) return setError(error)
+        setSuccessMessage("Sign up successful, please check your email to activate your account.")
+        setInput(baseInput)
     }
 
     function handleChange(e) {
@@ -28,30 +37,35 @@ function SignUp() {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-6">
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
-                    <input type="email" id="email" name="email" onChange={handleChange} value={input.email} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="email@gmail.com" required />
-                </div>
-                <div className="mb-6">
-                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
-                    <input type="password" id="password" name="password" onChange={handleChange} value={input.password} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-                </div>
-                <div className="mb-6">
-                    <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Confirm Password</label>
-                    <input type="password" id="repeat-password" name="repeat-password" onChange={handleChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
-                </div>
-                <div className="flex items-start mb-6">
-                    <div className="flex items-center h-5">
-                        <input id="terms" type="checkbox" value="" className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="" />
+            <div className='p-6'>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-6">
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email</label>
+                        <input type="email" id="email" name="email" onChange={handleChange} value={input.email} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="email@gmail.com" required />
                     </div>
-                    <label htmlFor="terms" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" className="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
-                </div>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
-            </form>
-            <p>
-                Already have an account? <Link to="/login">Log In</Link>
-            </p>
+                    <div className="mb-6">
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Password</label>
+                        <input type="password" id="password" name="password" onChange={handleChange} value={input.password} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                    </div>
+                    <div className="mb-6">
+                        <label htmlFor="repeat-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Confirm Password</label>
+                        <input type="password" id="repeat-password" name="repeatPassword" onChange={handleChange} value={input.repeatPassword} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" />
+                    </div>
+                    <div className="flex items-start mb-6">
+                        <div className="flex items-center h-5">
+                            <input id="terms" type="checkbox" value="" className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="" />
+                        </div>
+                        <label htmlFor="terms" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree that Olly and Hal are tapped and lost in space</label>
+                    </div>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+                    <p>
+                        Already have an account? <Link to="/login">Log In</Link>
+                    </p>
+                </form>
+            </div>
+
+            {error && <PopUpMessage message={error} messageType={"error"} setter={setError} />}
+            {sucessMessage && <PopUpMessage message={sucessMessage} messageType={"success"} setter={setSuccessMessage} />}
         </>
     )
 }
