9cb85a687f5f910ebacc72d69fbfe82a73a72b72
Tom Virgo <t.virgo@iwoca.co.uk>
2022-08-20 11:25:44 +0100

    add friend request notifications

--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,8 +1,12 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../../lib/api"
+import { PopUpMessage } from '..'
 
-const UserCard = ({ profileData }) => {
+const UserCard = ({ profileData, user }) => {
     const [img, setImg] = useState(false)
+    const [message, setMessage] = useState("")
+    const [messageType, setMessageType] = useState("success")
+    const [friendRequestSent, setFriendRequestSent] = useState(false)
 
     useEffect(() => {
         async function getProfilePicture() {
@@ -16,25 +20,61 @@ const UserCard = ({ profileData }) => {
         }
         getProfilePicture()
     }, [])
+
+    async function sendFriendRequest() {
+        async function createRequest() {
+            const { data, error } = await supabase
+                .from('friend_requests')
+                .insert([
+                    { to_user_id: profileData.user_id },
+                ])
+            if (error) {
+                setMessage("Unable to send friend request, contact support")
+                setMessageType("error")
+                return "error"
+            }
+            return data
+        }
+        async function createNotification() {
+            const { data, error } = await supabase
+                .from('notifications')
+                .insert([
+                    { for_user: profileData.user_id, notification_type: "friend_request", message: `You have a new friend request from ${user.user_metadata.first_name} ${user.user_metadata.last_name}` },
+                ])
+            if (error) {
+                setMessage("Unable to create notification, contact support")
+                setMessageType("error")
+                return "error"
+            }
+            return data
+        }
+        const [friend_request, notification] = await Promise.all([createRequest(), createNotification()]);
+        console.log(friend_request);
+        console.log(notification);
+        setMessage(`Friend request has been sent to ${profileData.first_name}, do not try again.`)
+        setMessageType("success")
+        setFriendRequestSent(true)
+    }
     return (
-        <div className="m-3 p-3 shadow-lg">
+        <div className="m-3 p-3 shadow-lg flex">
             {img ?
-                <div className="avatar">
+                <div className="avatar mr-3">
                     <div className="w-16 rounded">
                         <img src={URL.createObjectURL(img)} />
                     </div>
                 </div>
                 :
-                <div className="avatar placeholder">
-                    <div className="bg-neutral-focus text-neutral-content rounded-full w-24">
+                <div className="avatar placeholder mr-3">
+                    <div className="bg-neutral-focus text-neutral-content rounded-full w-20">
                         <span className="text-3xl">{profileData.first_name[0]}{profileData.last_name[0]}</span>
                     </div>
                 </div>
             }
             <div>
                 <h2 className="text-xl font-bold text-gray-500">{profileData.first_name} "{profileData.nickname}" {profileData.last_name}</h2>
-                <button className="btn btn-primary">Add friend</button>
+                <button className="btn btn-primary mt-1" disabled={friendRequestSent ? "disabled" : ""} onClick={sendFriendRequest}>Add friend</button>
             </div>
+            {message && <PopUpMessage message={message} messageType={messageType} setter={setMessage} />}
         </div>
     )
 }
--- src/pages/Friends/index.js
+++ src/pages/Friends/index.js
@@ -1,12 +1,18 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../../lib/api"
 import { Nav, UserCard } from "../../components";
+import { useAuth } from "../../contexts/auth";
+
 
 const Friends = () => {
     const [users, setUsers] = useState([])
+    const [showUserList, setShowUserList] = useState(true)
+    const [showFriendsList, setShowFriendsList] = useState(true)
     const [message, setMessage] = useState("")
     const [messageType, setMessageType] = useState("success")
     const [search, setSearch] = useState("")
+    const { user } = useAuth()
+
 
     useEffect(() => {
         async function fetchUsers() {
@@ -18,13 +24,37 @@ const Friends = () => {
         fetchUsers()
     }, [])
 
-    let userCards = users.map((u, i) => <UserCard profileData={u} key={i} />)
+    function toggleUserList() {
+        setShowUserList(prev => !prev)
+    }
+
+    function toggleFriendList() {
+        setShowFriendsList(prev => !prev)
+    }
+
+    let userCards = users.map((u, i) => <UserCard profileData={u} user={user} key={i} />)
 
     return (
         <>
             <Nav />
-            <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:tracking-tight sm:truncate ml-4">User List</h2>
-            {userCards}
+            <div className="flex">
+                <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:tracking-tight sm:truncate ml-4">User List</h2>
+                <div className="form-control">
+                    <label className="label cursor-pointer ml-2">
+                        <input type="checkbox" className="toggle toggle-secondary" onChange={toggleUserList} checked={showUserList} />
+                    </label>
+                </div>
+            </div>
+            {showUserList && <input type="text" placeholder="Search for user..." className="input w-full max-w-xs ml-3" />}
+            {showUserList && userCards}
+            <div className="flex">
+                <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:tracking-tight sm:truncate ml-4">Friends List</h2>
+                <div className="form-control">
+                    <label className="label cursor-pointer ml-2">
+                        <input type="checkbox" className="toggle toggle-secondary" onChange={toggleFriendList} checked={showFriendsList} />
+                    </label>
+                </div>
+            </div>
         </>
     )
 }
